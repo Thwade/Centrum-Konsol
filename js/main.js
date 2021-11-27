c0e955c58e0f15acb99be2eaf95daea34c98939b
Thwade <34820171+Thwade@users.noreply.github.com>
2021-11-27 03:52:26 +0100

    added to PhpStorm

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2021 Thwade. All rights reserved.
+ * Discord: Thwade#0001
+ * GitHub: https://github.com/Thwade
+ */
+
 $(document).ready(function(){
     $('.header').height($(window).height());
 })
